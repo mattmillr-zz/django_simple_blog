2a018a811e48db5857626503de295e659eab5923
Matt Miller <matt@brooklynsoftworks.com>
2012-02-05 23:00:24 -0500

    Debugging django app

--- urls.py
+++ urls.py
@@ -4,6 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', 'django_hello_server.simple_blog.views.index', name='simple_blog_index'),
+    url(r'^$', 'simple_blog.views.index', name='simple_blog_index'),
     url(r'^admin/', include(admin.site.urls)),
 )
